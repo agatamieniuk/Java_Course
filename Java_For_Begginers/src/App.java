072abd7978378259586872282cc9d492d692a4fd
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-29 13:07:18 +0200

    Task 17

--- Java_For_Begginers/src/App.java
+++ Java_For_Begginers/src/App.java
@@ -1,27 +1,20 @@
 
 public class App {
     public static void main(String[] args) {
-        getNotEvenSum(5);
-        getNotEvenSum(7);
-
+    getArray(4);
+    getArray(1);
     }
-
-    public static int getNotEvenSum(int a) {
-        int sum = 0;
-        if (a < 20) {
-            for (int i = 0; i < a; i++) {
-                if (i % 2 == 1) {
-                    continue;
-                }
-                sum = sum + i;
-            }
-            System.out.println(sum);
-        } else if (a > 20) {
-            System.out.println("Za duza liczba");
-        } else {
-            System.out.println("Liczba równa 20.");
-        } return a;
+    public static int[] getArray(int a){
+        int[] newTab = new int[10];
+        for (int i = 0; i < newTab.length; i++) {
+            newTab[i] = a + i;
+            System.out.println(newTab[i]);
+        } return newTab;
     }
 }
+
+
+
+
 
 
