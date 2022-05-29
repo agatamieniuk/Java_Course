01cf6e010dd886f85a3a50e883fa27bf40d6f84e
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-29 11:32:24 +0200

    Task 15

--- Java_For_Begginers/src/App.java
+++ Java_For_Begginers/src/App.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
-    Double a;
-    Double b;
         Scanner scanner = new Scanner(System.in);
-        a = scanner.nextDouble();
-        b = scanner.nextDouble();
-        System.out.println(a+b);
+
+        Car car = new Car();
+        System.out.println("Set car name:");
+        car.setName(scanner.nextLine());
+        System.out.println("Now car name is: " + car.getName());
+
+        Engine engine = new Engine();
+        System.out.println("Set engine power: ");
+        engine.setPower(scanner.nextInt());
+        car.setEngine(engine);
+        System.out.println("Now engine power is: " + car.getEngine().getPower());
+
+
+
 
     }
 }
