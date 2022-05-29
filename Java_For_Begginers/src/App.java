4393d2bb54d3c612e015b853d55bcd91dd9990d1
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-29 19:06:29 +0200

    Task 24b

--- Java_For_Begginers/src/App.java
+++ Java_For_Begginers/src/App.java
@@ -1,24 +1,30 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     private static final Task task = new Task();
     public static void main(String[] args) {
 
-        Car obj = CarFactory.createRandomCar();
+        List<Car> objList = new ArrayList<>();
+        objList.add(CarFactory.createRandomCar());
+        objList.add(CarFactory.createRandomCar());
 
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.setPrettyPrinting();
         Gson gson = gsonBuilder.create();
 
-        String json = gson.toJson(obj);
+        String json = gson.toJson(objList);
         System.out.println(json);
 
-        Car fromJson = gson.fromJson(json, Car.class);
+        TypeToken typeToken = new TypeToken<List<Car>>(){};
+        List<Car> fromJson = gson.fromJson(json, typeToken.getType());
+
         System.out.println(fromJson);
-        System.out.println(fromJson.equals(obj));
-
-
+        System.out.println(fromJson.equals(objList));
     }
 }
 
