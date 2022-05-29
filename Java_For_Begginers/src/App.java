0456af1ad6ac2ed5ef1251de9af1fce8625e55d0
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-29 13:15:44 +0200

    Task 18a

--- Java_For_Begginers/src/App.java
+++ Java_For_Begginers/src/App.java
@@ -1,15 +1,13 @@
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-    getArray(4);
-    getArray(1);
-    }
-    public static int[] getArray(int a){
-        int[] newTab = new int[10];
-        for (int i = 0; i < newTab.length; i++) {
-            newTab[i] = a + i;
-            System.out.println(newTab[i]);
-        } return newTab;
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(scanner.nextLine()).reverse();
+        String s = stringBuilder.toString();
+        System.out.println(s);
+
     }
 }
 
