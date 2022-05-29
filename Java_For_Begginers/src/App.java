ae070746965ae04ad701fa119697d687e4035623
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-29 15:40:22 +0200

    Task Files/Path ćwiczenie B

--- Java_For_Begginers/src/App.java
+++ Java_For_Begginers/src/App.java
@@ -1,32 +1,30 @@
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args) {
-        String fromFile = "";
-        Path path = Path.of("src", "Color.java");
-        try {
-            fromFile = Files.readString(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(fromFile);
-
+    public static void main(String[] args) throws IOException {
         Path path1 = Path.of("file.txt");
-//        try {
-//            Files.write(path1, fromFile.getBytes());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        try {
-            Files.writeString(path1, fromFile);
-        } catch (IOException e) {
-            e.printStackTrace();
+        Scanner scanner = new Scanner(System.in);
+        String line = "";
+        do {
+            line = line.concat(scanner.nextLine() + "\n");
+//            System.out.println(line);
+            Files.writeString(path1, line);
+
         }
+        while (!line.contains("#"));
+
+        List<String> strings = Files.readAllLines(path1);
+        for(String string: strings){
+            System.out.println(string);
+        }
+
+
+
     }
 }
 
