e5cb9cb496b392e86b863f42aae7a647effd8e05
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-29 14:16:49 +0200

    Task 22

--- Java_For_Begginers/src/App.java
+++ Java_For_Begginers/src/App.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        try {
-            double doubleParam = scanner.nextDouble();
-            System.out.println(doubleParam);
-        } catch (Exception e) {
-            System.out.println("Podaj liczbę!");
-        }
+        int intParam = 0;
+        boolean flag = true;
+
+        do {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                intParam = scanner.nextInt();
+                System.out.println(intParam);
+                flag = false;
+            } catch (Exception e) {
+                System.out.println("Podaj liczbę!");
+            }
+        }while(flag);
 
 
     }
