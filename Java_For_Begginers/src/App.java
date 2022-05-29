62c83490cda97e843797cc7b371454edaf7e8a92
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-29 09:48:24 +0200

    Task 7a

--- Java_For_Begginers/src/App.java
+++ Java_For_Begginers/src/App.java
@@ -1,21 +1,13 @@
 public class App {
     public static void main(String[] args) {
 
-        Engine engine = new Engine();
-        engine.setPower(1001);
-        engine.setCapacity(10001);
+        Student student1 = new Student("Kasia");
+        Student student2 = new Student("Zosia");
+        Student student3 = new Student("Ola");
 
-        //Car objects
-        Car car = new Car();
-        car.setName("Autko");
-        car.setMaxSpeed(100);
-        car.setEngine(engine);
-
-        System.out.println(car.getEngine().getCapacity());
-        System.out.println(car.getEngine().getPower());
-        System.out.println(car.getName());
-        System.out.println(car.getMaxSpeed());
-
+        student1.printName();
+        student2.printName();
+        student3.printName();
 
     }
 }
