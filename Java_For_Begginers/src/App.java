f8364ea4713a9198960aa6b9e80580e3523f4dbf
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-29 18:52:34 +0200

    Task 23b

--- Java_For_Begginers/src/App.java
+++ Java_For_Begginers/src/App.java
@@ -1,24 +1,38 @@
-
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
+    private static final Task task = new Task();
     public static void main(String[] args) throws IOException, InterruptedException {
 
         ObjectMapper objectMapper = new ObjectMapper();
         ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
 
         Car obj = CarFactory.createRandomCar();
-        String json = objectWriter.writeValueAsString(obj);
 
-        System.out.println(obj);
+        List<Car> cars = new ArrayList<>();
+        cars.add(obj);
+        cars.add(obj);
+
+        String json = objectWriter.writeValueAsString(cars);
+        for(Car car: cars){
+            System.out.println(car);
+        }
         System.out.println(json);
 
-        Car fromJson = objectMapper.readValue(json, Car.class);
+        TypeReference<List<Car>> typeReference = new TypeReference<>() {};
+        List<Car> fromJson = objectMapper.readValue(json, typeReference);
+
         System.out.println(fromJson);
-        System.out.println(obj.equals(fromJson));
+        System.out.println(cars.equals(fromJson));
+
+
 
     }
 }
