c524e33227e11bd97aeb4c2d95a0a4200572494a
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-29 12:11:53 +0200

    Task JavaSE * Scanner

--- Java_For_Begginers/src/App.java
+++ Java_For_Begginers/src/App.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        Car car = new Car();
-        System.out.println("Set car name:");
-        car.setName(scanner.nextLine());
-        System.out.println("Now car name is: " + car.getName());
-
-        Engine engine = new Engine();
-        System.out.println("Set engine power: ");
-        engine.setPower(scanner.nextInt());
-        car.setEngine(engine);
-        System.out.println("Now engine power is: " + car.getEngine().getPower());
-
+        double num = 1;
+        while(num % 3 != 0) {
+            num = scanner.nextDouble();
+            System.out.println("Podano liczbe: " + num);
+            if(num % 3 == 0){
+                System.out.println(num + " jest podzielna przez 3");
+            } else {
+                System.out.println(num + " NIE jest podzielna przez 3");
+            }
+            System.out.println("Wynik: " + num / 3);
+        }
 
 
 
