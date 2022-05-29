8a8b78087f4642e4f848d65872614f0b58f9e166
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-29 10:50:06 +0200

    Task 10

--- Java_For_Begginers/src/App.java
+++ Java_For_Begginers/src/App.java
@@ -1,15 +1,10 @@
 public class App {
     public static void main(String[] args) {
-    isNumberEven(3);
-    isNumberEven(2);
-    }
-    public static int isNumberEven(int i){
-        switch (i % 2) {
-            case 0:
-                System.out.println("Parzysta");
-                break;
-            case 1:
-                System.out.println("Nieparzysta");
-        }return i;
+
+        int i = 10;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
     }
 }
