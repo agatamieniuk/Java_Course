14c6b7786b11115f646323f89e0aa1c189074fdc
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-31 18:56:43 +0200

    Add Ex1.

---
+++ Homeworks/Ex1/src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    public static Double add(Double a, Double b){
+        return a+b;
+    }
+    public static Double sub(Double a, Double b){
+        return a-b;
+    }
+    public static Double mul(Double a, Double b){
+        return a*b;
+    }
+    public static Double div(Double a, Double b){
+        return a/b;
+    }
+}
