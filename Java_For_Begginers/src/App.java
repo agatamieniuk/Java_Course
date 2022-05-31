6e3a20e13215ec3f08902c98aebff0324cdc7a40
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-05-31 15:53:05 +0200

    Task A-Java: concat

--- Java_For_Begginers/src/App.java
+++ Java_For_Begginers/src/App.java
@@ -2,15 +2,14 @@ public class App {
     private static final Task task = new Task();
 
     public static void main(String[] args) {
-        System.out.println(App.addMultiply(2,7));
+        System.out.println(App.concatenate("info", "Share"));
+
+
     }
 
-    public static Integer addMultiply(Integer firstNum, Integer secondNum) {
-        Integer sum = 0;
-        for (int i = 1; i <= secondNum; i++){
-            sum = firstNum + sum;
-        }
-        return sum;
+    public static String concatenate(String firstWord, String secondWord) {
+        String concat = firstWord.concat(secondWord);
+        return concat;
     }
 }
 
