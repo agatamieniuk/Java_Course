ae748a4970240bd353b821407404101aad50d717
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-06-04 21:01:23 +0200

    Final method

--- OOP/calculation/src/calculation/Calculation.java
+++ OOP/calculation/src/calculation/Calculation.java
@@ -1,7 +1,7 @@
 package calculation;
 
 public interface Calculation {
-    //dostaniesz 2 wartosci i masz mi obliczyc coś na tej podstawie
+
     int calculate(int a, int b);
-    //to, co bedzie metody calculation - jedyne, co bedzie moglo zrobic, to zwrocic nam rezultat metody calculate();
+
 }
--- OOP/calculation/src/calculation/CalculationApp.java
+++ OOP/calculation/src/calculation/CalculationApp.java
@@ -10,18 +10,14 @@ public class CalculationApp {
         int b = 3;
 
         ValuesHolder holder = new ValuesHolder(a, b);
-        /*Implememtuje obiekt typu calculation. Obiekt typu calculation ma implementację add/sub (tabela, dwie wartosci)
-        Obiekt calculationStrategy, tego samego typu - Calculation - jest wielopostaciowy, moze byc Add, moze byc Sub - przykład POLIMORFIZMU*/
         Calculation[] calculationStrategy = {new Sub(), new Add()};
         for (Calculation calculation : calculationStrategy) {
-            //przekazanie obiektów do metody
             performCalculation(holder, calculation);
         }
 
     }
 
     private static void performCalculation(ValuesHolder holder, Calculation calculationStrategy) {
-        //informuję, jak ma zostać wywolana operacja
         int result = holder.getResult(calculationStrategy);
         System.out.printf("%d %s %d = %d%n", holder.getA(),calculationStrategy, holder.getB(), result);
     }
