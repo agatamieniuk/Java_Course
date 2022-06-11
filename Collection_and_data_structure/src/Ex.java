a6187b2092f363f034a8c7b402c703d49f655330
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-06-11 19:02:35 +0200

    Ex 1a - arrays

---
+++ Collection_and_data_structure/src/Ex.java
@@ -0,0 +1,13 @@
+public class Ex {
+    public static void ex1a() {
+        int[] tab = new int[10];
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = i;
+            System.out.println(tab[i]);
+        }
+    }
+
+    public static void ex() {
+
+    }
+}
---
+++ Collection_and_data_structure/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Ex.ex1a();
+    }
+}
