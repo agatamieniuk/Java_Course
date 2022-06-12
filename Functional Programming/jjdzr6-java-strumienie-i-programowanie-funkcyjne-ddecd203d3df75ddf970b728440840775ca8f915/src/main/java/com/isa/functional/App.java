bcf50112ea889f50fb3f92710d6ca1c330302c36
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-06-12 19:13:55 +0200

    Initial commit

---
+++ Functional Programming/jjdzr6-java-strumienie-i-programowanie-funkcyjne-ddecd203d3df75ddf970b728440840775ca8f915/src/main/java/com/isa/functional/App.java
@@ -0,0 +1,16 @@
+package com.isa.functional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger("App");
+        logger.info("hello world!");
+
+    }
+}
---
+++ Functional Programming/jjdzr6-java-strumienie-i-programowanie-funkcyjne-ddecd203d3df75ddf970b728440840775ca8f915/src/main/java/com/isa/functional/model/Address.java
@@ -0,0 +1,32 @@
+package com.isa.functional.model;
+
+public class Address {
+
+    private final String state;
+    private final String city;
+    private final String stret;
+    private final Integer population;
+
+    public Address(String state, String city, String stret, Integer population) {
+        this.state = state;
+        this.city = city;
+        this.stret = stret;
+        this.population = population;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStret() {
+        return stret;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+}
