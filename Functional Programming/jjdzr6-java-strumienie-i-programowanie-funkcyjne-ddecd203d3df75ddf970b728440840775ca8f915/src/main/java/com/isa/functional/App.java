bf9a9052a6738b9403e9802e2c2f49ba6c0e6419
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-06-12 20:24:57 +0200

    Interface funkcyjny z dwoma parametrami => myFunction<T, R, Z>

--- Functional Programming/jjdzr6-java-strumienie-i-programowanie-funkcyjne-ddecd203d3df75ddf970b728440840775ca8f915/src/main/java/com/isa/functional/App.java
+++ Functional Programming/jjdzr6-java-strumienie-i-programowanie-funkcyjne-ddecd203d3df75ddf970b728440840775ca8f915/src/main/java/com/isa/functional/App.java
@@ -1,6 +1,7 @@
 package com.isa.functional;
 
 import com.isa.functional.myfunctions.MyFirstFunction;
+import com.isa.functional.myfunctions.MyTwoParameterFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,21 +13,10 @@ public class App {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger("App");
 
-        //stworzenie funkcji z Interface'u MyFirstFunction (implementacja interfaceu)
-        MyFirstFunction<String, Integer> myFirstFunction = new MyFirstFunction<String, Integer>() { //new na Interface tworzy klasę anonimową
-            @Override
-            public Integer apply(String s) {
-                return s.length();
-            }
-        };
-        //to samo, co powyżej zapisane za pomocą Lambdy
-        MyFirstFunction<String, Integer> myFirstFunction2 = s -> s.length(); //na Stringu s zostaje wywołana metoda s.lenght();
-        Integer x = myFirstFunction2.apply("Ala ma kota");//<=uzycie funkcji (nasz Intiger, ktory zwracamy)
-        logger.info("Nasz integer: " + x);
+        MyTwoParameterFunction<Integer, Integer, Integer> myTwoParameterFunction = (a, b) -> a + b; //parametry umieszczamy w nawiasie
+        Integer sum = myTwoParameterFunction.apply(2, 3);
+        logger.info("Nasza suma: " + sum);
 
-        MyFirstFunction<String, String> mySecondFunction = s -> s + " drugi string";
-        String y = mySecondFunction.apply("Parametr zawierajacy pierwszy string i");
-        logger.info(y);
 
     }
 }
