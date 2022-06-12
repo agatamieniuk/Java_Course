d9621e01ef37e584a4f388d555d2b5f35ec33d6a
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-06-12 21:04:39 +0200

    Drugi przykład na interface ktory niczego nie zwraca

--- Functional Programming/jjdzr6-java-strumienie-i-programowanie-funkcyjne-ddecd203d3df75ddf970b728440840775ca8f915/src/main/java/com/isa/functional/App.java
+++ Functional Programming/jjdzr6-java-strumienie-i-programowanie-funkcyjne-ddecd203d3df75ddf970b728440840775ca8f915/src/main/java/com/isa/functional/App.java
@@ -2,9 +2,13 @@ package com.isa.functional;
 
 import com.isa.functional.myfunctions.MyConsumer;
 import com.isa.functional.myfunctions.MyFirstFunction;
+import com.isa.functional.myfunctions.MyProducer;
 import com.isa.functional.myfunctions.MyTwoParameterFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * Hello world!
@@ -14,7 +18,12 @@ public class App {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger("App");
 
-        MyConsumer<String> myConsumer = s-> logger.info(s);
-        myConsumer.apply("Hej");
+        MyProducer<String> myProducer = () -> "nie przyjmuje parametrów";
+        String apply = myProducer.apply();
+        logger.info(apply);
+
+        MyProducer<String> myProducer2 = () -> LocalDateTime.now().toString();
+        String localDate = myProducer2.apply();
+        logger.info(localDate);
     }
 }
