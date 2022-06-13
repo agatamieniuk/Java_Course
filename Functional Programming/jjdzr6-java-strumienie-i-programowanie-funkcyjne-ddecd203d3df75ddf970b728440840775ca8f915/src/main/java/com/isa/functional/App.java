f8c51e2f8f0e53702f18241c2dd233a99d790cd8
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-06-13 21:27:37 +0200

    Optional - jak pozbyc sie nulla

--- Functional Programming/jjdzr6-java-strumienie-i-programowanie-funkcyjne-ddecd203d3df75ddf970b728440840775ca8f915/src/main/java/com/isa/functional/App.java
+++ Functional Programming/jjdzr6-java-strumienie-i-programowanie-funkcyjne-ddecd203d3df75ddf970b728440840775ca8f915/src/main/java/com/isa/functional/App.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 /**
  * Hello world!
@@ -16,11 +17,21 @@ public class App {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger("App");
 
-        MyFilter<String> myFilter = (mojString -> mojString.length() % 2 == 0);
-        boolean czyParzysty = myFilter.test("Hej");
-        boolean czyParzysty2 = myFilter.test("He");
-        logger.info("Czy parzysty: " + czyParzysty);
-        logger.info("Czy parzysty: " + czyParzysty2);
+        String x = null;
+        Optional<String> mabyeX = Optional.ofNullable(x);
+        Optional<Integer> mabyeInteger = mabyeX.map(value -> value.length());
+        logger.info(mabyeX.toString()); //=<pokazuje empty bo Optional usunął nulla
+        logger.info(mabyeInteger.toString());
+        String y = "Agat";
+        Optional<String> mabyeY = Optional.ofNullable(y);
+        logger.info(mabyeY.toString());//podal wartosc Optional[Agata]
+
+        Optional<Integer> mabyeInteger2 = mabyeY
+                .map(value -> value.length())
+                .filter(value->value%2==0);
+        logger.info(mabyeInteger2.toString());
+        logger.info(mabyeInteger2.orElse(0) + " "); //zwroc mi wartosc a jesli jej nie ma orElse(), zwroc mi zero
+        logger.info(mabyeInteger2.orElseGet(()->0).toString());// to samo co wyzej tylko zapisane przez lambde
 
     }
 }
