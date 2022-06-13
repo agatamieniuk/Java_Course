63699f4cd4ed8f0a91d0d8f2d55c59ea371b5174
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-06-13 20:17:15 +0200

    Flatmap

--- Functional Programming/jjdzr6-java-strumienie-i-programowanie-funkcyjne-ddecd203d3df75ddf970b728440840775ca8f915/src/main/java/com/isa/functional/containters/Container.java
+++ Functional Programming/jjdzr6-java-strumienie-i-programowanie-funkcyjne-ddecd203d3df75ddf970b728440840775ca8f915/src/main/java/com/isa/functional/containters/Container.java
@@ -1,11 +1,12 @@
 package com.isa.functional.containters;
+
 import com.isa.functional.myfunctions.MyFirstFunction;
 
-public class Container <T>{
+public class Container<T> {
     private final T value; //<-final, bo w programowaniu funkcyjnym dane mają być niezmienne
 
     //MAP => zamienia  elementy jedne w drugie
-    public <U>Container<U> map(MyFirstFunction<T,U> mapper){//<=metoda map zwraca nam Container<U> za pomocą metody zawartej w nawiasie,
+    public <U> Container<U> map(MyFirstFunction<T, U> mapper) {//<=metoda map zwraca nam Container<U> za pomocą metody zawartej w nawiasie,
         return new Container<>(mapper.apply(value));       //  (i tylko ta metoda w nawiasie nas interesuje), tj. chce tu wrzucic funkcje
                                                            //  MyFirstFunction<T,U>(z parametrem T, zwracajacą U) i ta metoda nazywa się mapper
     }
@@ -18,4 +19,9 @@ public class Container <T>{
         return value;
     }
 
+    //FLATMAP => przyjmuje jeden element/jedną wartość a zwraca wiele elementów/wiele wartości
+    // uzywane np. przy Liście list (Lista jako parametr i w srodku kilka list jako dane zwracane)
+    public <U> Container<U> flatmap(MyFirstFunction<T, Container<U>> mapper) {//jako parametr wejsciowy przyjmie T a zwroci Container<U> czyli kilka U
+        return mapper.apply(value);
+    }
 }
