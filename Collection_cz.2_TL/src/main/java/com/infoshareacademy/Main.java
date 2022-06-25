3280b711387a538a8630409e91842dd53e1e64a0
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-06-25 20:13:25 +0200

    Cwiczenie 1b - Car[]

--- Collection_cz.2_TL/src/main/java/com/infoshareacademy/Main.java
+++ Collection_cz.2_TL/src/main/java/com/infoshareacademy/Main.java
@@ -1,6 +1,8 @@
 package com.infoshareacademy;
 
 import com.infoshareacademy.exercises.Ex;
+import com.infoshareacademy.factories.CarFactory;
+import com.infoshareacademy.model.vehicle.Car;
 
 import java.util.*;
 
@@ -8,19 +10,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Integer[] table = ex1a(1, 2, 3, 4);
+        CarFactory car = new CarFactory();
+        Car randomCar = car.createRandomCar();
+        Car randomCar2 = car.createRandomCar();
+        Car randomCar3 = car.createRandomCar();
 
-        for(Integer tables: table){
-            System.out.println(tables);
+        Car[] cars = ex1b(randomCar, randomCar2, randomCar3);
+
+        System.out.println(randomCar);
+        System.out.println(randomCar2);
+        System.out.println(randomCar3);
+        System.out.println("-------------");
+        for (Car random : cars) {
+            System.out.println(random);
         }
+
+
     }
 
-    public static Integer[] ex1a(Integer...params) {
+    public static Integer[] ex1a(Integer... params) {
         Integer[] tab = new Integer[params.length];
 
-         for(int i = 0; i < params.length; i++){
-            tab[i] = params[i]*2;
-        }return tab;
+        for (int i = 0; i < params.length; i++) {
+            tab[i] = params[i] * 2;
+        }
+        return tab;
+    }
+
+    public static Car[] ex1b(Car... cars) {
+        Car[] carTab = new Car[cars.length];
+
+        for (int i = 0; i < cars.length; i++) {
+            carTab[i] = cars[i];
+        }
+        return carTab;
+    }
+
+    public static void ex() {
     }
 
 }
