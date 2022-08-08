82a6e909049c7a0ad94cfdd91aeafce722016adc
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-08-08 20:43:25 +0200

    Exercises form

--- Spring - Thymeleaf/src/main/java/com/example/web/controller/ProductController.java
+++ Spring - Thymeleaf/src/main/java/com/example/web/controller/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
@@ -28,5 +30,18 @@ public class ProductController {
     public String getProducts(Model model) {
         model.addAttribute("products", productService.findAll());
         return "products";
+    }
+
+    @GetMapping("/products/new")
+    public String getProductForm(Model model){
+        model.addAttribute("product", new ProductDto());
+        return "product-form";
+    }
+
+    @PostMapping("/products/new")
+    public String sendProduct(@ModelAttribute("product") ProductDto product){
+        productService.create(product);
+        return "product-form-success";
+
     }
 }
