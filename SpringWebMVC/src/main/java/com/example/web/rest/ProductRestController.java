572e891f0eea4b680786e571dbca5b067c699398
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-08-11 19:24:57 +0200

    @RequestMapping use.

--- SpringWebMVC/src/main/java/com/example/web/rest/ProductRestController.java
+++ SpringWebMVC/src/main/java/com/example/web/rest/ProductRestController.java
@@ -3,12 +3,14 @@ package com.example.web.rest;
 import com.example.web.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/products/clothes")
 public class ProductRestController {
 
-    @GetMapping("api/products/clothes/best")
+    @GetMapping("best")
     public ProductDto getTheBest(){
         ProductDto productDto = new ProductDto();
         productDto.setId(1L);
