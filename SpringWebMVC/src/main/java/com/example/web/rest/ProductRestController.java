5dc99d636c09fdddb16aa01b9676b0df1629e0f1
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-08-11 20:10:56 +0200

    @PathVariable use.

--- SpringWebMVC/src/main/java/com/example/web/rest/ProductRestController.java
+++ SpringWebMVC/src/main/java/com/example/web/rest/ProductRestController.java
@@ -3,6 +3,7 @@ package com.example.web.rest;
 import com.example.web.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,14 @@ public class ProductRestController {
         ProductDto productDto = new ProductDto();
         productDto.setId(1L);
         productDto.setName("best product");
+        return productDto;
+    }
+
+    @GetMapping("t-shirts/{id}")
+    public ProductDto getProduct(@PathVariable Long id) { //nazwa jak nazwa zmiennej z klasy Product
+        ProductDto productDto = new ProductDto();
+        productDto.setId(id);
+        productDto.setName("cool t-shirt");
         return productDto;
     }
 }
