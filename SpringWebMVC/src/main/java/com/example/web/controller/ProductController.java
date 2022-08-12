7b4f6c74fe78bb3e6ec5f3d446c53df375791550
Agata Mieniuk <dzikipaczekbialostocki@gmail.com>
2022-08-12 21:16:31 +0200

    MVC example - Thymeleaf page with {id} and {name}

--- SpringWebMVC/src/main/java/com/example/web/controller/ProductController.java
+++ SpringWebMVC/src/main/java/com/example/web/controller/ProductController.java
@@ -1,15 +1,31 @@
 package com.example.web.controller;
 
+import com.example.web.dto.ProductDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("products")
 public class ProductController {
 
-    @GetMapping
-    public String getProduct(){
+    @GetMapping("model/{id}")
+    public String getProduct(@PathVariable Long id, @RequestParam(required = false, name = "name", defaultValue = "koszulka")String name,
+                             Model model){
+        ProductDto productDto = createProduct(id, name);
+        model.addAttribute("product", productDto); //pierwsze to nazwa nadana przez nas (ktora musi byc zgodna z tym co jest w Thymeleafowym odnosniku(!) np.<h2 th:text="'Product [id=' + ${product.id} + ']'"></h2>,
+                                                                // drugie to wartosc zmiennej ktora chcemy pobrac/wyswietlic
+        model.addAttribute("title", "Model product Page");
         return "product";
+    }
+
+    private ProductDto createProduct(Long id, String name){
+        ProductDto productDto = new ProductDto();
+        productDto.setId(id);
+        productDto.setName(name);
+        return productDto;
     }
 }
